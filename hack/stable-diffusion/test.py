104284264d077236e98cd4df10fe29f8749041d0
Gabe Conradi <gabe.conradi@gmail.com>
2022-08-23 19:23:12 -0400

    make script into class

--- hack/stable-diffusion/test.py
+++ hack/stable-diffusion/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+from typing import Dict
 import torch
 import time
 import yaml
@@ -7,95 +8,108 @@ import secrets
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 
-prompt = "a butterfly's markings swirling into the chaotic void of the horsehead nebulae"
+#prompt = "a butterfly's markings swirling into the chaotic void of the horsehead nebulae"
+prompt = "Federation Starship Enterprise NCC-1701-d blueprint"
 #prompt = "an executive helicopter departing from the corporate headquarters at night"
 #prompt = "a cheerful cartoon bear greeting a family at the trail in a national forest"
-count=1 # how many images to generate
+iterations=5
+batch_size=1 # how many images to generate
 
-output_dir = "output"
-model_id = "CompVis/stable-diffusion-v1-4"
 cuda_conf_env_var = "PYTORCH_CUDA_ALLOC_CONF"
-device = "cuda"
-height=384
-width=512
-num_inference_steps=50 # 50 default, number of denoising steps
-guidance_scale=7 # 7 or 8.5, scale for classifier-free guidance
+#height=384
+#width=512
+#num_inference_steps=50 # 50 default, number of denoising steps
+#guidance_scale=8.5 # 7.5 default, (7-8.5 reasonable?) , scale for classifier-free guidance
 cuda_max_split_size_mb = 64
 initial_seed = secrets.randbits(64) # None
-output_id = secrets.token_hex(16)
-torch_pipeline_precision = torch.float32
-cuda_conf = ""
 
 ## Setup and Configuration
-if torch.cuda.is_available():
-    device = "cuda"
-else:
-    device = "cpu"
-
-print("initializing {} ({})...".format(output_id, device))
-gpu_mem = torch.cuda.mem_get_info()[0] # bytes
-if height%8!=0 or width%8!=0:
-    raise "height and width need to be divisible by 8"
-
-if cuda_conf_env_var in os.environ:
-    cuda_conf = os.environ[cuda_conf_env_var]
-
-if gpu_mem < 10*(1024**3): # check gpu vmem < 10GB, use float16
-    torch_pipeline_precision = torch.float16
-    if cuda_conf == "":
-        cuda_conf = "max_split_size_mb:{max_split_size_mb}".format(max_split_size_mb=cuda_max_split_size_mb)
-
-os.environ[cuda_conf_env_var] = cuda_conf
-
-generator = torch.Generator(device)
-if initial_seed is not None:
-    generator = generator.manual_seed(initial_seed)
-
-#print("memory_stats(): {stats}\nmemory_summary(): {summary}".format(stats=torch.cuda.memory_stats(), summary=torch.cuda.memory_summary()))
-
-print("prompt: {} ({}x{}, count: {})".format(prompt, width, height, count))
-
-md = {
-        'id': output_id,
-        'seed': generator.initial_seed(),
-        'settings': {
-            'num_inference_steps': num_inference_steps,
-            'guidance_scale': guidance_scale,
-            'height': height,
-            'width': width,
-            'count': count,
-            #'torch': {
-            #    'precision': torch_pipeline_precision,
-            #    'foo': 'bar',
-            #},
-        },
-        'model_id': model_id,
-        'prompt': prompt,
-        'timestamp': time.time(),
-        'output':  'TODO: image encoding as base64',
-        'self': "{}.yaml".format(output_id),
-}
-
 def write_metadata(output_file, metadata):
     f = open(output_file, 'w')
     f.write(yaml.dump(metadata))
     f.close()
 
-with autocast(device):
-    pipe = StableDiffusionPipeline.from_pretrained(
-            model_id,
-            use_auth_token=True,
-            torch_dtype=torch_pipeline_precision).to(device)
+class KRunner:
+    def __init__(self, settings = {}) -> None:
+        self.output_dir = "output"
+        self.settings = {
+            'model_id': "CompVis/stable-diffusion-v1-4",
+            'num_inference_steps': 50,
+            'guidance_scale': 8.5,
+            'height': 384,
+            'width': 512,
+            'batch_size': 1,
+        } | settings
 
-    try:
-        images = pipe([prompt]*count, generator=generator, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale, height=height, width=width)["sample"]
-        for i in list(range(len(images))):
-            image = images[i]
-            fname = "output-{ts}-{id}-{idx}.png".format(id=md['id'], ts=int(md['timestamp']), idx=i)
-            image.save(os.path.join(output_dir, fname))
-            print("view with `xdg-open {}` (metadata: {})".format(fname, md['self']))
-    except RuntimeError as e:
-        md['error'] = e
-        print("caught error: {}", e)
+        self.width = self.settings['width']
+        self.height = self.settings['height']
+       
+        if torch.cuda.is_available():
+            self.device = "cuda"
+        else:
+            self.device = "cpu"
 
-write_metadata(os.path.join(output_dir, md['self']), md)
+        gpu_mem = torch.cuda.mem_get_info()[0] # bytes
+        if self.height%8!=0 or self.width%8!=0:
+            raise ValueError("height and width need to be divisible by 8")
+
+        cuda_conf = ""
+        if cuda_conf_env_var in os.environ:
+            cuda_conf = os.environ[cuda_conf_env_var]
+
+        self.torch_pipeline_precision = torch.float32
+        if gpu_mem < 10*(1024**3): # check gpu vmem < 10GB, use float16
+            self.torch_pipeline_precision = torch.float16
+            if cuda_conf == "":
+                cuda_conf = "max_split_size_mb:{max_split_size_mb}".format(max_split_size_mb=cuda_max_split_size_mb)
+
+        os.environ[cuda_conf_env_var] = cuda_conf
+
+        self.generator = torch.Generator(self.device)
+        if initial_seed is not None:
+            self.generator = self.generator.manual_seed(initial_seed)
+        #print("initialized!")
+        #print("memory_stats(): {stats}\nmemory_summary(): {summary}".format(stats=torch.cuda.memory_stats(), summary=torch.cuda.memory_summary()))
+
+    def infer(self, prompt, batch_size):
+        model_id = self.settings['model_id']
+        with autocast(self.device):
+            pipe = StableDiffusionPipeline.from_pretrained(
+                    model_id,
+                    use_auth_token=True,
+                    torch_dtype=self.torch_pipeline_precision).to(self.device)
+
+            for iteration in list(range(0, iterations)):
+                if iterations > 1:
+                    print("inferrence iteration {}".format(iteration))
+
+                print("prompt: {} ({}x{}, batch_size: {})".format(prompt, self.width, self.height, batch_size))
+                ident = secrets.token_hex(16)
+                md = {
+                        'id': ident,
+                        'settings': self.settings,
+                        'runtime': {
+                            'model_id': model_id,
+                            'seed': self.generator.initial_seed(),
+                        },
+                        'prompt': prompt,
+                        'timestamp': time.time(),
+                        'output':  'TODO: image encoding as base64',
+                        'self': "{}.yaml".format(ident),
+                }
+
+                try:
+                    images = pipe([prompt]*batch_size, generator=self.generator, num_inference_steps=self.settings['num_inference_steps'], guidance_scale=self.settings['guidance_scale'], height=self.settings['height'], width=self.settings['width'])["sample"]
+                    for i in list(range(len(images))):
+                        image = images[i]
+                        fname = "output-{ts}-{id}-{idx}.png".format(id=md['id'], ts=int(md['timestamp']), idx=i)
+                        image.save(os.path.join(self.output_dir, fname))
+                        print("view with `xdg-open {}` (metadata: {})".format(fname, md['self']))
+                except RuntimeError as e:
+                    md['error'] = e
+                    print("caught error: {}", e)
+
+                write_metadata(os.path.join(self.output_dir, md['id'] + ".yaml"), md)
+
+app = KRunner()
+app.infer(prompt, batch_size)
